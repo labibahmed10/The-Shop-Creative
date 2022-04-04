c6cd784d26e93293be2edefc31caa73b84b156a1
labibahmed10 <labib.ahmed.372@gmail.com>
2022-04-04 17:30:12 +0600

    deployed to netlify and also fixed error

--- src/Components/ReviewPage/ReviewPage.js
+++ src/Components/ReviewPage/ReviewPage.js
@@ -5,10 +5,13 @@ import Review from "../Review/Review";
 const ReviewPage = () => {
   const [reviews, setReviews] = useReview();
   return (
-    <div className="grid grid-cols-2 gap-6 py-16">
-      {reviews.map((review) => (
-        <Review key={review.id} review={review}></Review>
-      ))}
+    <div>
+      <h1 className="text-center pt-12 text-3xl text-[#779c84]">What Our Valuable Customer Says</h1>
+      <div className="grid grid-cols-2 gap-6 py-12">
+        {reviews.map((review) => (
+          <Review key={review.id} review={review}></Review>
+        ))}
+      </div>
     </div>
   );
 };
